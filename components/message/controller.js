ad53efa9ee1c6cc75c5a199664b8c0da04dc91a3
Jhon Arcila Castaño <jdeiber@uno27.co>
2021-12-20 18:00:43 -0500

    estructura node

--- components/message/controller.js
+++ components/message/controller.js
@@ -1,9 +1,32 @@
-
+const store = require("./store");
 
 function addMessage(user, message) {
-    
+  return new Promise((resolve, reject) => {
+    if (!user || !message) {
+      console.error("Error: faltan datos");
+      return reject("Invalid data");
+    }
+    const fullMessage = {
+      user: user,
+      message: message,
+      date: new Date(),
+    };
+
+    console.log(fullMessage);
+    //agregamos el mensaje al store
+    store.add(fullMessage);
+    resolve(fullMessage);
+  });
+}
+
+function getMessage() {
+  new Promise((resolve, reject) => {
+    resolve(store.list());
+  });
+  return store.list();
 }
 
 module.exports = {
-    addMessage
-}
+  addMessage,
+  getMessage,
+};
--- components/message/network.js
+++ components/message/network.js
@@ -4,21 +4,25 @@ const controller = require('./controller')
 const response = require('../../network/response');
 
 router.get('/', function (req, res) {
-    console.log(req.headers);
-    res.header("Access-Control-Allow-Origin", "valor personalizado");
-    //res.send('Hola desde get!');
-    response.success(req, res, 'Lista de mensajes');
+    controller.getMessage().then((message) => {
+        response.success(req, res, message, 200)
+    }).catch((err) => {
+        response.error(req, res, 'error: ', 500, err)
+    })
 });
 
-router.post('/', function (req, res) {
-    //res.status(201).send({error: '', body: "creado correctamante"});
-    if (req.query.error == 'ok') {
-        response.error(req, res, 'Error simulado al crear el mensaje', 500, 'Es una simulacion de los errores');
-    } else { 
-        response.success(req, res, 'Mensaje creado', 201);
+router.post("/", async (req, res) => {
+
+    const { user, message } = req.body
+
+    try {
+        const fullMessage = await controller.addMessage(user, message)
+        response.success(req, res, fullMessage, 201)
+    } catch (error) {
+        response.error(req, res, "Informacion invalida", 500, "Error en el contenido")
     }
-});
 
+})
 
 router.delete('/', function (req, res) {
     console.log(req.body);
