bf2d0c43a3ca4757888a1c509440956110b6c968
Jhon Arcila Castaño <jdeiber@uno27.co>
2021-12-22 15:14:37 -0500

    chat modelo,rutas,network,etc

--- components/chat/model.js
+++ components/chat/model.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const mySchema = new Schema({
     users: [{
         type: Schema.ObjectId,
-        ref: User
-    }]
+        ref: 'User',
+    }],
 });
 
-const model = mongoose.model("Chat", mySchema);
+const model = mongoose.model('Chat', mySchema);
 
 module.exports = model;
